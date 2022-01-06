641b78280bd35662f24a370913f8b62c17e0099a
Jacob Mendt <jacob.mendt@pikobytes.de>
2022-01-06 20:18:37 +0100

    Migration to typo3 10.4
    Add local changes!
    
    Upgrade typo3 version
    
    Migration to typo3 10.4
    
    Migrate to typo3 10.4

--- public/typo3conf/LocalConfiguration.php
+++ public/typo3conf/LocalConfiguration.php
@@ -18,14 +18,7 @@ return [
             ],
         ],
     ],
-    'EXT' => [
-        'extConf' => [
-            'backend' => 'a:6:{s:14:"backendFavicon";s:0:"";s:11:"backendLogo";s:0:"";s:20:"loginBackgroundImage";s:0:"";s:13:"loginFootnote";s:0:"";s:19:"loginHighlightColor";s:0:"";s:9:"loginLogo";s:0:"";}',
-            'extensionmanager' => 'a:2:{s:21:"automaticInstallation";s:1:"1";s:11:"offlineMode";s:1:"0";}',
-            'scheduler' => 'a:2:{s:11:"maxLifetime";s:4:"1440";s:15:"showSampleTasks";s:1:"1";}',
-            'slub_web_kartenforum' => 'a:8:{s:8:"basemaps";s:624:"[{"id":"slub-osm","label":"SLUB OSM","urls":["https://basemaps-1.pikobytes.de/styles/maptiler-basic-v2/{z}/{x}/{y}.png","https://basemaps-2.pikobytes.de/styles/maptiler-basic-v2/{z}/{x}/{y}.png","https://basemaps-3.pikobytes.de/styles/maptiler-basic-v2/{z}/{x}/{y}.png"],"type":"xyz","tileSize":256},{"id":"maptiler-satellite","label":"MapTiler Satellite","urls":["https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=9wghD5zlvdu13A81zHqB"],"type":"xyz","tileSize":512},{"id":"bkg-topoplus","label":"BKG TopoPlus","urls":["https://sgx.geodatenzentrum.de/wms_topplus_open"],"type":"wms", "layers": "web", "tileSize":512}]";s:7:"mapView";s:106:"{ "extent": [-20026376.39,-20048966.10,20026375.39,20048966.10], "center": [1528150, 6630500], "zoom": 2 }";s:12:"urlNominatim";s:46:"https://kartenforum.slub-dresden.de/nominatim/";s:9:"urlSearch";s:37:"https://search-slub.pikobytes.de/vk20";s:10:"urlTerrain";s:79:"https://api.maptiler.com/tiles/terrain-quantized-mesh/?key=kRAKrA0wcbZZFOT64bX5";s:9:"georefApi";s:29:"https://geo-slub.pikobytes.de";s:14:"georefAuthUser";s:0:"";s:18:"georefAuthPassword";s:0:"";}',
-        ],
-    ],
+    'EXT' => [],
     'EXTENSIONS' => [
         'backend' => [
             'backendFavicon' => '',
@@ -39,18 +32,14 @@ return [
             'automaticInstallation' => '1',
             'offlineMode' => '0',
         ],
-        'scheduler' => [
-            'maxLifetime' => '1440',
-            'showSampleTasks' => '1',
-        ],
         'slub_web_kartenforum' => [
-            'basemaps' => '[{"id":"slub-osm","label":"SLUB OSM","urls":["https://basemaps-1.pikobytes.de/styles/maptiler-basic-v2/{z}/{x}/{y}.png","https://basemaps-2.pikobytes.de/styles/maptiler-basic-v2/{z}/{x}/{y}.png","https://basemaps-3.pikobytes.de/styles/maptiler-basic-v2/{z}/{x}/{y}.png"],"type":"xyz","tileSize":256},{"id":"maptiler-satellite","label":"MapTiler Satellite","urls":["https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=9wghD5zlvdu13A81zHqB"],"type":"xyz","tileSize":512},{"id":"bkg-topoplus","label":"BKG TopoPlus","urls":["https://sgx.geodatenzentrum.de/wms_topplus_open"],"type":"wms", "layers": "web", "tileSize":512}]',
-            'georefApi' => 'https://geo-slub.pikobytes.de',
+            'basemaps' => '[{"id":"slub-osm","label":"SLUB OSM","urls":["https://tile-1.kartenforum.slub-dresden.de/styles/maptiler-basic-v2/{z}/{x}/{y}.png","https://tile-2.kartenforum.slub-dresden.de/styles/maptiler-basic-v2/{z}/{x}/{y}.png"],"type":"xyz","tileSize":256},{"id":"maptiler-satellite","label":"MapTiler Satellite","urls":["https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=9wghD5zlvdu13A81zHqB"],"type":"xyz","tileSize":512},{"id":"bkg-topoplus","label":"BKG TopoPlus","urls":["https://sgx.geodatenzentrum.de/wms_topplus_open"],"type":"wms", "layers": "web", "tileSize":512}]',
+            'georefApi' => 'https://geo.kartenforum.slub-dresden.de',
             'georefAuthPassword' => '',
             'georefAuthUser' => '',
             'mapView' => '{ "extent": [-20026376.39,-20048966.10,20026375.39,20048966.10], "center": [1528150, 6630500], "zoom": 2 }',
             'urlNominatim' => 'https://kartenforum.slub-dresden.de/nominatim/',
-            'urlSearch' => 'https://search-slub.pikobytes.de/vk20',
+            'urlSearch' => 'https://search.kartenforum.slub-dresden.de/vk20',
             'urlTerrain' => 'https://api.maptiler.com/tiles/terrain-quantized-mesh/?key=kRAKrA0wcbZZFOT64bX5',
         ],
     ],
@@ -104,6 +93,5 @@ return [
             'unifiedPageTranslationHandling' => true,
         ],
         'sitename' => 'New TYPO3 site',
-        'systemLogLevel' => 2,
     ],
 ];
